cad2b96d34dca4ed1b2e424dc82008480f2025c1
kaito2625 <kaitoa863@gmail.com>
2023-08-17 11:42:57 +0900

    ブログ投稿一覧の表示条件変更

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [PostController::class, 'index']);
 
 Route::get('/posts',[PostController::class, 'index']);
+
